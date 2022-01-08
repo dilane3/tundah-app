5eed3527b7ba90896f1e9f5363de7882c7258856
dilane3 <komboudilane125@gmail.com>
2022-01-08 01:10:39 +0100

    updating the signup page

--- client/src/components/marketing/pageSections/signin/secondConnexionPage.jsx
+++ client/src/components/marketing/pageSections/signin/secondConnexionPage.jsx
@@ -113,7 +113,7 @@ const SecondConnexionPage = () => {
 							value={credentials.username}
 							id="username"
 							handleChange={handleChange}
-							placeholder="Votre adresse username"
+							placeholder="Votre username"
 						/>
 					</div>
 					<div className="w-full">
@@ -123,7 +123,7 @@ const SecondConnexionPage = () => {
 							value={credentials.password}
 							id="password"
 							handleChange={handleChange}
-							placeholder="Mot de passe"
+							placeholder="Votre mot de passe"
 						/>
 					</div>
 					
@@ -133,7 +133,7 @@ const SecondConnexionPage = () => {
 						className="disabled:bg-gray-300 disabled:text-gray-500 disabled:cursor-not-allowed w-full bg-primary hover:bg-primary-hover px-2 py-2 lg:px-3 lg:py-2.5 text-center text-white text-base md:text-xl rounded" 
 						onClick={handleSubmit}
 					/>
-					<div className="flex flex-row justify-start py-3">
+					<div className="flex flex-row justify-end py-3">
 						Pas encore inscrit ? 
 						<ALink link="/signup" classe="inline-block ml-2">inscrivez vous</ALink>
 					</div>
