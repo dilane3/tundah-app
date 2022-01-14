f98f757051f8bb3b255d26f368cdcd2cf9680c77
Fenyep <tenordubois10@gmail.com>
2022-01-14 19:52:09 +0100

    Post Feature: Possibility to make a research from the mobile viewport menu

--- client/src/components/marketing/navbar/MobileMenu.jsx
+++ client/src/components/marketing/navbar/MobileMenu.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BsSearch } from "react-icons/bs";
+import { Link } from 'react-router-dom'
 import styles from "../../../css/mobileMenu.module.css"
 import Input from "../../elements/input/Input";
 import Aside from "../aside/Aside";
 
 const MobileMenu = ({show}) => {
+
+  const [researchQuery, setResearchQuery] = useState("")
+
+	const handleChange = event => {
+
+		event.preventDefault();
+
+		setResearchQuery(event.target.value);
+	}	
+
+	useEffect(() => {
+		console.log("The value entered is: ", researchQuery)
+	}, [researchQuery])
+
   return (
     <section className={`${styles.mobileMenuSection} ${show ? styles.mobileMenuAnimation:''}`}>
       <div className={styles.mobileMenuSearchEngine}>
 				<Input 
 					type="search"
-					placeholder="Faites une recherche..."	
+					placeholder="Faites une recherche..."
+					handleChange={handleChange}
 				/>
 
-        <BsSearch />
+        <Link to={{
+            pathname: '/search',
+            state: {
+            researchQuery: researchQuery
+          }
+        }}>
+					<div className={styles.headerSearchEngineIcon}>
+						<BsSearch />
+					</div>
+				</Link>
 			</div>
 
       <Aside />
