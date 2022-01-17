2c6052f69a5b6fc53a11e2e11c4a732b99ee06ec
Fenyep <tenordubois10@gmail.com>
2022-01-17 13:04:53 +0100

    Creation and usage of my instance and ressourceUrl variable

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -8,19 +8,25 @@ import axios from "axios";
 // 	baseURL: "http://192.168.43.81:5000/api",
 // })
 
-const instance = axios.create({
-	baseURL: "https://tundah-server.herokuapp.com/api"
-})
-
 // const instance = axios.create({
-// 	baseURL: "http://localhost:3001/api/posts"
+// 	baseURL: "https://tundah-server.herokuapp.com/api"
 // })
 
+const instance = axios.create({
+	baseURL: "http://localhost:3001/api"
+})
+
 const ressourcesUrl = {
-	profil: "https://tundah-server.herokuapp.com/static/images/profil",
-	postImages: "https://tundah-server.herokuapp.com/static/images/post",
-	postVideos: "https://tundah-server.herokuapp.com/static/videos"
+	profil: "http://localhost:3001/static/images/profil",
+	postImages: "http://localhost:3001/static/images/post",
+	postVideos: "http://localhost:3001/static/videos"
 }
+
+// const ressourcesUrl = {
+// 	profil: "https://tundah-server.herokuapp.com/static/images/profil",
+// 	postImages: "https://tundah-server.herokuapp.com/static/images/post",
+// 	postVideos: "https://tundah-server.herokuapp.com/static/videos"
+// }
 
 // const ressourcesUrl = {
 // 	profil: "http://192.168.43.81:5000/static/images/profil",
