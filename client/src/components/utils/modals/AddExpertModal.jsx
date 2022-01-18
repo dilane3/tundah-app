b659d8cd51616b68b72b17a4bbc17d155446a5f1
Blondelle-sudo <blondellekana0@gmail.com>
2022-01-18 11:11:07 +0100

    addeExpert page

--- client/src/components/utils/modals/AddExpertModal.jsx
+++ client/src/components/utils/modals/AddExpertModal.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { BsX } from 'react-icons/bs'
 import styles from '../../../css/addExpert.module.css'
 import AddExpertContent from './AddExpertContent'
 
 const AddExpertModal = ({onHide, animationClass}) => {
 
+  const[newExpert,setNewExpert]= useState("");
+
+  const handleChange = event =>{
+    setNewExpert(event.currentTarget.value);
+}
+
+
   return (
     <section className={`${styles.addExpertSection} ${animationClass ? styles.addExpertSectionAnimation:null}`}>
- 
-        <div className={styles.addExpertFrom}>
-          <input 
-            type="text"
-            placeholder="nom d'utilisateur d'un abonne"
-          />
+        <div className={styles.addExpertHeader}>
+          <span > Gestion des Experts </span>
+          <div className={styles.addExpertFrom}>
+            <input 
+              type="text"c
+              placeholder="nom d'utilisateur d'un abonne"
+              onChange={handleChange}
+            />        
+            
+          </div>
+
+          <span className={styles.addExpertClose} onClick={onHide}>
+            <BsX className="icon" size="25" />
+          </span>
         </div>
-
-        <span className={styles.addExpertClose} onClick={onHide}>
-          <BsX className="icon" size="25" />
-        </span>
-
       <div className={styles.addExpertContent}>
+        <AddExpertContent/>
+        <AddExpertContent/>
+        <AddExpertContent/>
+        <AddExpertContent/>
         <AddExpertContent/>
       </div>
     </section>
