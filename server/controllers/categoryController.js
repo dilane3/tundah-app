9f8eb5d35db55fb26707bb479b05abad52170fdb
ngimdock <ngimdockzemfack@gmail.com>
2022-06-04 07:36:06 +0200

    Initialisation des cathégories

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,4 +1,5 @@
 import { config } from "dotenv";
+import CategoryModel from "../models/CategoryModel.js"
 
 // fetching data from .env file
 config();
@@ -7,6 +8,25 @@ const { SECRET_CODE_TOKEN } = process.env;
 
 class CategoryController {
 
+    static async createCathegory(req, res){
+        
+        const { name } = req.body
+
+        if(!name || name.length < 5) return res.status(400).json({ error: "Name of the cathegory to short" })
+
+        name = name.trim()
+
+        try{
+            const result = await CategoryModel.createCathegory(name)
+
+            if(result) return res.status(201).json({ result })
+        }catch(err){
+            return { error: err }
+        }
+    }
 }
+
+setSearrch(value)
+
 
 export default CategoryController
--- server/models/CategoryModel.js
+++ server/models/CategoryModel.js
@@ -5,6 +5,46 @@ import InterfaceCategoryModel from "./interfaces/interfaceCategoryModel.js";
 
 class CategoryModel extends InterfaceCategoryModel {
 
+    static async getAllCathegory(){
+        const session = dbConnect()
+
+        const query = `
+            MATCH (cathegories:Cathegory)
+            return cathegories
+            ORDER BY cathegories.names
+        `
+
+        try{
+            const result = session.run(query)
+            console.log(result);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    static async createCathegory(name) {
+        const session = dbConnect()
+
+        const query = `
+            CREATE(cathegory:Cathegory){
+                id: $id,
+                name: $name
+            }
+            RETURN cathegory
+        `
+
+        try{
+            const result = await session.run(query, {
+                id: nanoid(20),
+                name
+            })
+
+            console.log(result)
+        }catch(err){
+            console.log(err)
+            return{ error: "And error occured while creating the cathegory" }
+        }
+    }
 }
 
 export default CategoryModel
