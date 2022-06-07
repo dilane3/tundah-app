85cb1390f06fbd33055c9acfad2ab58ba2646a2b
Fenyep <tenordubois10@gmail.com>
2022-06-07 15:52:48 +0100

    NOTHING: I just change the server API call adress

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({
-	baseURL: "http://localhost:5000/api"
-})
+// const instance = axios.create({
+// 	baseURL: "http://localhost:3001/api"
+// })
 
 // const instance = axios.create({
 // 	baseURL: "http://192.168.43.81:5000/api",
@@ -12,15 +12,15 @@ const instance = axios.create({
 // 	baseURL: "https://tundah-server.osc-fr1.scalingo.io/api"
 // })
 
-// const instance = axios.create({
-// 	baseURL: "http://localhost:3001/api"
-// })
+const instance = axios.create({
+	baseURL: "http://localhost:3001/api"
+})
 
-// const ressourcesUrl = {
-// 	profil: "http://localhost:3001/static/images/profil",
-// 	postImages: "http://localhost:3001/static/images/post",
-// 	postVideos: "http://localhost:3001/static/videos"
-// }
+const ressourcesUrl = {
+	profil: "http://localhost:3001/static/images/profil",
+	postImages: "http://localhost:3001/static/images/post",
+	postVideos: "http://localhost:3001/static/videos"
+}
 
 // const ressourcesUrl = {
 // 	profil: "https://tundah-server.osc-fr1.scalingo.io/static/images/profil",
@@ -34,10 +34,10 @@ const instance = axios.create({
 // 	postVideos: "http://192.168.43.81:5000/static/videos"
 // }
 
-const ressourcesUrl = {
-	profil: "http://localhost:5000/static/images/profil",
-	postImages: "http://localhost:5000/static/images/post",
-	postVideos: "http://localhost:5000/static/videos"
-}
+// const ressourcesUrl = {
+// 	profil: "http://localhost:5000/static/images/profil",
+// 	postImages: "http://localhost:5000/static/images/post",
+// 	postVideos: "http://localhost:5000/static/videos"
+// }
 
 export {instance, ressourcesUrl}
