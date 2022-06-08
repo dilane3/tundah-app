0e1b0caceb55a1231fb7ed380eda48ef3b068fb0
ngimdock <ngimdockzemfack@gmail.com>
2022-06-08 13:06:05 +0100

    Get single cathegory with the givent id mode OK

--- server/models/CategoryModel.js
+++ server/models/CategoryModel.js
@@ -35,7 +35,110 @@ class CategoryModel extends InterfaceCategoryModel {
         }
     }
 
+    /**
+     * This function get cathegory with the given id
+     * @param {id} id 
+     * @returns cathegory | error
+     */
+    static getCathegory = async (id) => {
+        const session = dbConnect()
 
+        const query = `
+            MATCH (cathegory:Cathegory{ id: $id })
+            return cathegory
+        `
+
+        try{
+            const result = await session.run(query, { id })
+
+            if (result.records.length > 0){
+                const cathegoryData = result.records[0].get("cathegory").properties
+
+                return { data: cathegoryData }
+            }
+            return { error: "Error while creating an user" };
+        }catch(err){
+            console.log(err)
+            return{ error: "And error occured while geting the cathégory" }
+        }finally{
+            session.close()
+        }
+    }
+
+    /**
+     * This function create a cathegory
+     * @param {id} id 
+     * @param {name} name 
+     * @returns data | error
+     */
+    static createCathegory = async ({ id, name }) => {
+        const session = dbConnect()
+
+        const query = `
+            CREATE(cathegory:Cathegory{
+                id: $id,
+                name: $name
+            })
+            RETURN cathegory
+        `
+
+        try{
+            const result = await session.run(query, { id, name })
+
+            if (result.records.length > 0){
+                const cathegoryData = result.records[0].get("cathegory").properties
+
+                return { data: cathegoryData }
+            }
+            return { error: "Error while creating an user" };
+        }catch(err){
+            console.log(err)
+            return { error: "Error while creating an user" };
+        }finally{
+            session.close()
+        }
+    }
+
+    /**
+     * This function delete cathegory 
+     * @param {id} id 
+     * @returns cathegory | error
+     */
+    static deleteCathegory = async (id) => {
+        const session = dbConnect()
+
+        const query = `
+            MATCH(cathegory:Cathegory{ id: $id })
+            DETACH DELETE cathegory
+        `
+        try{
+            const result = await session.run(query, { id })
+
+            if (result.records.length > 0){
+                const cathegoryData = result.records[0].get("cathegory").properties
+
+                return { data: cathegoryData }
+            }
+            return { error: "Error while deleting a cathegory" };
+        }catch(err){
+            console.log(err)
+
+            return { error: "And occured while deleting the cathegory" }
+        }finally{
+            session.close()
+        }
+    }
+
+    static updateCathegory = async ({id, name}) => {
+        const session = dbConnect()
+
+        const query = `
+            MATCH(cathegory:Cathegory{ id: $id })
+            SET
+                cathegory.id: $id
+                cathegory.name: $name
+        `
+    }
 }
 
 export default CategoryModel
