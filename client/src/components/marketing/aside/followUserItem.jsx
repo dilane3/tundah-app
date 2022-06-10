2ae73fca8c2a238fbf1fa60635f900cdd2b917a0
dilane3 <komboudilane125@gmail.com>
2022-06-10 22:46:13 +0100

    ADD: Follow user from the Followers Suggestion List Ok

--- client/src/components/marketing/aside/followUserItem.jsx
+++ client/src/components/marketing/aside/followUserItem.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import ImgCircle from '../../elements/imgCircle/ImgCircle'
-import { BiPlus } from 'react-icons/bi'
+import { BiMinus, BiPlus } from 'react-icons/bi'
 import './followUserStyle.css'
 import { formatName } from "../../../utils/format";
 import { ressourcesUrl } from "../../../utils/url";
 import { getRelativeDate } from "../../../utils/dateOperations";
+import currentUserContext from "../../../dataManager/context/currentUserContent";
+import UserApi from "../../../api/users";
+import FollowersSuggestionContext from "../../../dataManager/context/followersSuggestioinContext";
 
 const FollowUserItem = ({ data }) => {
+  // Get data from global state
+  const { currentUser, addFollowing, deleteFollowing } = useContext(currentUserContext)
+  const { removeSuggestion } = useContext(FollowersSuggestionContext)
+
+  // Follow and unFollow a user
+  const handleFollowUser = async (e, user) => {
+    e.preventDefault()
+
+    try {
+      const type = currentUser.alreadyFollowed(user.getId) ? "unfollow" : "follow"
+
+      // Handle follow action before sending the request
+      if (type === "follow") {
+        addFollowing(user)
+
+        const timer = setTimeout(() => {
+          removeSuggestion(user.getId)
+
+          clearTimeout(timer)
+        }, 3000)
+      } else {
+        deleteFollowing(user.getId)
+      }
+
+      const { data, error } = await UserApi.follow({ type, userId: user.getId })
+
+      if (!data) {
+        console.log(error)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="followcontent">
       <div className="followUserAction">
@@ -18,8 +55,17 @@ const FollowUserItem = ({ data }) => {
           <span className="flwUserInscription">{getRelativeDate(data.getDate)} </span>
         </div>
       </div>
-      <button className="btnSuivreFlw">
-        <BiPlus color="3c6a46" size={15} />suivre
+      <button className="btnSuivreFlw" onClick={(e) => handleFollowUser(e, data)}>
+        {
+          currentUser.alreadyFollowed(data.getId) ? (
+            <>
+              <BiMinus color="3c6a46" size={15} />annule
+            </>
+          ) : (
+            <>
+              <BiPlus color="3c6a46" size={15} />suivre
+            </>
+          )}
       </button>
     </div>
   )
