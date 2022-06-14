1e4781d5eeb4c2455005b7e90f886b3cf9973b13
dilane3 <komboudilane125@gmail.com>
2022-06-14 09:38:11 +0100

    Production

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -4,13 +4,13 @@ import axios from "axios";
 //   baseURL: "http://localhost:5000/api",
 // });
 
-const instance = axios.create({
-  baseURL: "http://192.168.43.81:5000/api",
-});
-
 // const instance = axios.create({
-//   baseURL: "https://tundah-server.herokuapp.com/api",
+//   baseURL: "http://192.168.43.81:5000/api",
 // });
+
+const instance = axios.create({
+  baseURL: "https://tundah-server.herokuapp.com/api",
+});
 
 // const instance = axios.create({
 // 	baseURL: "http://localhost:3001/api"
@@ -22,17 +22,17 @@ const instance = axios.create({
 // 	postVideos: "http://localhost:3001/static/videos"
 // }
 
-// const ressourcesUrl = {
-//   profil: "https://tundah-server.herokuapp.com/static/images/profil",
-//   postImages: "https://tundah-server.herokuapp.com/static/images/post",
-//   postVideos: "https://tundah-server.herokuapp.com/static/videos",
-// };
-
 const ressourcesUrl = {
-  profil: "http://192.168.43.81:5000/static/images/profil",
-  postImages: "http://192.168.43.81:5000/static/images/post",
-  postVideos: "http://192.168.43.81:5000/static/videos",
+  profil: "https://tundah-server.herokuapp.com/static/images/profil",
+  postImages: "https://tundah-server.herokuapp.com/static/images/post",
+  postVideos: "https://tundah-server.herokuapp.com/static/videos",
 };
+
+// const ressourcesUrl = {
+//   profil: "http://192.168.43.81:5000/static/images/profil",
+//   postImages: "http://192.168.43.81:5000/static/images/post",
+//   postVideos: "http://192.168.43.81:5000/static/videos",
+// };
 
 // const ressourcesUrl = {
 //   profil: "http://localhost:5000/static/images/profil",
