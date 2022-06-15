8cef6521e4554b8832b2beb85bcd10263b9544d2
Fenyep <tenordubois10@gmail.com>
2022-06-15 07:08:28 +0100

    ADD: Addition and implementation of the getUserFollowedCategories() method to retrieve the categories followed by the current user

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -71,6 +71,34 @@ class CategoryController {
             return { error }
         }
     }
+
+    static getUserFollowedCategories = async({id} ,req, res) => { 
+
+        // const user = req.user
+
+        // const { value } = req.params
+
+        console.log(id)
+
+        try {
+            if(id) {
+                const { data, error } = await CategoryModel.getUserCategoriesFollowed(id.value)
+                
+                return { data }
+                // if(data.length > 0) {
+                //     return res.status(200).json(data)
+                // } else {
+                //     return res.status(500).json({ message: "Server Internal Error" })
+                // }
+            } else {
+                return res.status(400).json({ message: "The user identifier is needed" })
+            }
+        } catch(error) {
+            console.log(error)
+            return { error }
+        }
+
+    }
 }
 
 export default CategoryController;
